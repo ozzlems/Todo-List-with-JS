e872ccc627d10ce0f813c7ef36369ab12338ccdb
ozzlems <ozlemsencoruh@gmail.com>
2023-04-02 15:18:57 +0300

    added mark and trash buttons

--- main.js
+++ main.js
@@ -18,8 +18,16 @@ const newTodo = document.createElement('li');
 newTodo.innerText = 'hey';
 newTodo.classList.add('todo-item');
 todoDiv.appendChild(newTodo);
-
+//check mark button
 const completedButton = document.createElement('button');
+completedButton.innerHTML = '<i class = "fas fa-check"></i>'
+completedButton.classList.add("complete-btn");
+todoDiv.appendChild(completedButton);
+//check trash button
+const trashButton = document.createElement('button');
+trashButton.innerHTML = '<i class = "fas fa-trash "></i>'
+trashButton.classList.add("complete-btn");
+todoDiv.appendChild(trashButton);
 
-
+todoList.appendChild(todoDiv);
 }
