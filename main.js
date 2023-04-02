07caf4095cde2fc992a29e1009680cd47b276154
ozzlems <ozlemsencoruh@gmail.com>
2023-04-02 18:06:35 +0300

    styled buttons

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ todoDiv.appendChild(completedButton);
 //check trash button
 const trashButton = document.createElement('button');
 trashButton.innerHTML = '<i class = "fas fa-trash "></i>'
-trashButton.classList.add("complete-btn");
+trashButton.classList.add("trash-btn");
 todoDiv.appendChild(trashButton);
 
 todoList.appendChild(todoDiv);
