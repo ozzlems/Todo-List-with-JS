92478e52b4982afb3d57bd4efc48ab65af0db60f
ozzlems <ozlemsencoruh@gmail.com>
2023-04-03 02:19:05 +0300

    modified js but selection is not working

--- main.js
+++ main.js
@@ -2,10 +2,11 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
-
+const filterOption = document.querySelector('.filter-todo');
 //event listener
 todoButton.addEventListener('click' , addTodo);
 todoList.addEventListener('click' , deleteCheck);
+filterOption.addEventListener('click', filterTodo);
 
 
 
@@ -53,4 +54,30 @@ if(item.classList[0] === 'complete-btn'){
     todo.classList.toggle('completed');
     
 }
+}
+
+function filterTodo(e){
+const todos = todoList.childNodes;
+todos.forEach(function(todo){
+    switch(e.target.value){
+        case "all":
+           todo.style.display = "flex";
+           break;
+            case "completed":
+                if(todo.classList.contains('completed')){
+                    todo.style.display = "flex";
+                }
+                else{
+                    todo.style.display = "none";
+                }
+            case "uncompleted":
+                if(!todo.classList.contains('completed')){
+                    todo.style.display = "flex";
+                }    
+                else{
+                    todo.style.display = "none";
+                }
+
+    }
+})
 }
