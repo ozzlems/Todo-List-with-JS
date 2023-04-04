25157e1bcd23e8a790798f5bc1a0654c42532450
ozzlems <ozlemsencoruh@gmail.com>
2023-04-04 23:57:54 +0300

    added local storage

--- main.js
+++ main.js
@@ -83,3 +83,16 @@ todos.forEach(function(todo){
     }
 })
 }
+
+
+function saveLocalTodos(todo){
+    let todos;
+    if(localStorage.getItem('todos') === null){
+        todos = [];
+    }
+    else{
+        todos = JSON.parse(localStorage.getItem('todos'));
+    }
+    todos.push(todo);
+    localStorage.setItem('todos' , JSON.stringify(todos));
+}
